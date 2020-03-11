0ca12c80c9fc46a047cf4d08d0e9e94f173ccb17
danielyoo98 <dyoo9@my.bcit.ca>
2020-03-10 18:42:35 -0700

    set up basic project structure

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const route = require('./routes');
+const expressHbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.engine(
+    'hbs',
+    expressHbs({
+        layoutsDir: 'views/layouts/',
+        defaultLayout: 'main-layout',
+        extname: 'hbs'
+    })
+);
+
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 app.use(route);
 
