08ab79ba74488073ffd0ed89c5d3f54af1fd6e4b
Stella Tran <stellatranthi@hotmail.com>
2020-03-12 13:36:22 -0700

    Added initial database files

---
+++ models/peopleData.js
@@ -0,0 +1,50 @@
+let db = require('../db/db');
+
+function addPeople(e) {
+     db.query("Insert into people (name,about,imageurl,dob,country) VALUES ('" + e.name +"','"+ e.about + "','"+ e.url +"','"+ e.dob + "','"+ e.country + "')");
+}
+
+function getAllPeople() {
+    return db.query('Select * from people');
+}
+
+function getPeople(id) {
+    return db.query('Select * from people where id = ' + id);
+}
+
+function getContacts(id){
+    return db.query('select * from contacts left join people on people.id = contacts.user_id where user_id = ' + id);
+}
+
+function getConversation(id,contactid){
+    return db.query('select conversation_id from conversation where first_contact =' + id + ' and second_contact =' + contactid + ' or first_contact=' +contactid+ 'and second_contact=' + id);
+}
+
+function getMessages(id){
+    return db.query('select message from message where conversation_id =' + id)
+}
+
+function addContact(id,addid){
+    return db.query('INSERT INTO contacts (user_id,contact_id) VALUES (' + id + ',' + addid + '),(' + addid + ',' + id + ');')
+}
+
+function writeMessage(conversationid,senderid,,datetime,message){
+    return db.query('INSERT INTO message (conversation_id,sender,message,datetime) VALUES (' + conversationid + ', ' + senderid +','+ message +',' + datetime+');')
+}
+
+function createConversation(id,receiverid){
+    return db.query('INSERT INTO public.conversation (first_contact,second_contact) VALUES (' + id + ','+receiverid +');')
+}
+
+
+module.exports = {
+    add : addPeople,
+    getall : getAllPeople,
+    getpeople: getPeople,
+    getconvo:getConversation,
+    getmessages:getMessages,
+    getcontacts:getContacts,
+    addcontact:addContact,
+    writemessage:writeMessage,
+    createconvo:createConversation
+}
