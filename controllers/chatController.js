a12a8aef2176b3c788fd8a6a9cd3f1ee7c8a1048
Stella Tran <stellatranthi@hotmail.com>
2020-03-13 16:07:10 -0700

    Started using templating to populate elements on page

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -2,7 +2,12 @@ let mod = require('../models/peopleData');
 
 
 function loadChat(req,res,next) {
-    res.render('chat', {chatAssests: true});
+    // let user_id = req.body.user_id;
+    
+    let dbQuery = mod.getcontacts(1);
+    dbQuery.then((data) => {
+        res.render('chat', {chatAssests: true, contacts: data.rows});
+    }).catch(err => console.log(err));  
 }
 
 module.exports = {
