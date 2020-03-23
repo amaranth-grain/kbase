95ae871fffa8b86d85422c7d027847e05f637b7a
John Lee <johnlee46@gmail.com>
2020-03-23 10:41:07 -0700

    update chatdata.js
    updated

--- models/chatData.js
+++ models/chatData.js
@@ -1,5 +1,17 @@
 let db = require('../db/db');
 
+function addPeople(e) {
+     db.query("Insert into people (name,about,imageurl,dob,country) VALUES ('" + e.name +"','"+ e.about + "','"+ e.url +"','"+ e.dob + "','"+ e.country + "')");
+}
+
+function getAllPeople() {
+    return db.query('Select * from people');
+}
+
+function getPeople(id) {
+    return db.query('Select * from people where id = ' + id);
+}
+
 //gets a list of people that a user has conversations with 
 function getContacts(id){
     return db.query('(select * from conversation left join people on people.id = conversation.second_person where first_person ='+id +' ) union all (select * from  conversation left join people on people.id = conversation.first_person where second_person = '+ id+')')
@@ -28,7 +40,7 @@ function getConversation(id,contactid){
 
 //gets messages for the specified conversation id 
 function getMessages(conversation_id){
-    return db.query('select message from message where conversation_id =' + conversation_id)
+    return db.query('select * from message where conversation_id =' + conversation_id)
 }
 
 //inserts a message into the message table 
@@ -43,6 +55,9 @@ function createConversation(id,receiverid){
 
 
 module.exports = {
+    add : addPeople,
+    getall : getAllPeople,
+    getpeople: getPeople,
     getconvo: getConversation,
     getmessages: getMessages,
     getcontacts: getContacts,
