a3d23136d7bd722800730a3612a6f5a056bc33ba
John Lee <johnlee46@gmail.com>
2020-03-23 10:33:02 -0700

    renaming of files and modification of methods
    renaming of files and modification of methods messages selects all columns

--- models/chatData.js
+++ models/chatData.js
@@ -1,17 +1,5 @@
 let db = require('../db/db');
 
-function addPeople(e) {
-     db.query("Insert into people (name,about,imageurl,dob,country) VALUES ('" + e.name +"','"+ e.about + "','"+ e.url +"','"+ e.dob + "','"+ e.country + "')");
-}
-
-function getAllPeople() {
-    return db.query('Select * from people');
-}
-
-function getPeople(id) {
-    return db.query('Select * from people where id = ' + id);
-}
-
 //gets a list of people that a user has conversations with 
 function getContacts(id){
     return db.query('(select * from conversation left join people on people.id = conversation.second_person where first_person ='+id +' ) union all (select * from  conversation left join people on people.id = conversation.first_person where second_person = '+ id+')')
@@ -52,32 +40,14 @@ function createMessage(conversationid,senderid,datetime,message){
 function createConversation(id,receiverid){
     return db.query('INSERT INTO conversation (first_contact,second_contact) VALUES (' + id + ','+receiverid +');')
 }
-//creates a discussion row in the table, sample datime 2017-03-04 06:08:00
-function createDiscussion(id,details,datetime,tag){
-    return db.query('insert into discussion (person_id,details,datetime,tag) values (' +id+ ',' +details + ','+ datetime+', '+tag+');')
-}
-
-function createReply(person_id,discussion_id,reply_details,reply_time){
-    return db.query('insert into reply (person_id,discussion_id,reply_details,reply_time) values ('+person_id+','+discussion_id+','+reply_details+',' +reply_time+');')
-}
-
-//gets the replies of a discussion based on the discussion id
-function getReplies(discussion_id){
-    return db.query('select reply_details from reply where discussion_id = '+discussion_id+' order by reply_time;')
-}
 
 
 module.exports = {
-    add : addPeople,
-    getall : getAllPeople,
-    getpeople: getPeople,
     getconvo: getConversation,
     getmessages: getMessages,
     getcontacts: getContacts,
     addcontact: addContact,
     createmessage: createMessage,
     createconvo: createConversation,
-    creatediscussion:createDiscussion,
-    createreply: createReply,
-    getreplies: getreplies
+
 }
