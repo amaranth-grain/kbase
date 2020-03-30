652ff6e01a821e36426f09741df9240fc0bfa682
Stella Tran <stellatranthi@hotmail.com>
2020-03-30 01:51:36 -0700

    Messages are now accurate

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -10,54 +10,57 @@ function loadConversation(req,res,next) {
     //
     //
     var user_id = 1;
-    var queryConv = mod.getmessages(res.locals.convId);
+    var conservationId = res.locals.convId;
+    
+    var queryConv = mod.getmessages(conservationId);
 
     queryConv
     .then((data) => {
         newMessages = data.rows;
         newMessages.forEach(element => {
-            let date = element.date;
+            let date = new Date(Date.parse(element.timestamp + "+0000"));
             element.date = `${date.toLocaleString('default', { month: 'short' })} ${date.getDate()}`;
             element.time = date.toLocaleString('default', { hour: 'numeric', hour12: true, minute: 'numeric'});
         });
-        res.render('chat', {chatAssests: true, contacts: res.locals.contacts, messages: newMessages, convId: res.locals.convId, sender: user_id});
-    })
+        res.render('chat', {chatAssests: true, contacts: res.locals.contacts, messages: newMessages, convId: conservationId, sender: user_id});
+    }).catch(err => console.log(err));  
+
 }
 
 function getContacts(req,res,next) {
     // let user_id = req.body.user_id;
     let user_id = 1;
-    console.log("TEST: " + req.body.convId)
     let dbQuery = mod.getcontacts(user_id);
     dbQuery.then((data) => {
         res.locals.contacts = data.rows;
-        // console.log(res.locals.contacts)
         next();
     }).catch(err => console.log(err));  
 }
 
 function getConvId(req,res,next) {
-    // let user_id = req.body.user_id;
-    // REPLACE WITH SESSIONS... 
-    //
-    //
-    //
-    //
-    //
-    //
-    var user_id = 1;
-
-    var contact_id = req.query.contactId;
-    if(contact_id == undefined){
-        contact_id = res.locals.contacts[0].id;
-    }
-
-    var queryConv = mod.getconvo(user_id, contact_id);
-    queryConv
-    .then((data) => {
-        res.locals.convId = (data.rows[0]["conversation_id"]);
+    if(req.body.convId != undefined){
+        res.locals.convId = req.body.convId;
         next();
-    }).catch(err => console.log(err));  
+    } else {
+        // let user_id = req.body.user_id;
+        // REPLACE WITH SESSIONS... 
+        //
+        //
+        //
+        //
+        //
+        //
+        var user_id = 1;
+
+        var contact_id = res.locals.contacts[0].id;
+
+        var queryConv = mod.getconvo(user_id, contact_id);
+        queryConv
+        .then((data) => {
+            res.locals.convId = data.rows[0]["conversation_id"];
+            next();
+        }).catch(err => console.log(err));  
+    }
 }
 
 function newMessage(req,res,next) {
