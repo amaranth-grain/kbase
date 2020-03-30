5ac009f5a0bba1ed28937ca0fcc168945b679c24
Stella Tran <stellatranthi@hotmail.com>
2020-03-29 23:56:59 -0700

    Decoupled controller methods

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
 
-router.get('/chat', chatController.loadChat);
-
-router.post('/chat/loadConversation', chatController.loadConversation);
+router.get('/chat', chatController.getContacts, chatController.getConvId, chatController.loadConversation);
 
 router.post('/chat/newMessage', chatController.newMessage);
 
