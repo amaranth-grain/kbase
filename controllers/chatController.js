a29d982f491fb6c59fc7465f29e33e0c40235568
Stella Tran <stellatranthi@hotmail.com>
2020-03-29 23:56:59 -0700

    Decoupled controller methods

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -2,40 +2,58 @@ let mod = require('../models/chatData');
 
 function loadConversation(req,res,next) {
     // let user_id = req.body.user_id;
+    // REPLACE WITH SESSIONS... 
+    //
+    //
+    //
+    //
+    //
+    //
     var user_id = 1;
-    var contact_id = req.body.contactId;
-    var newContacts = [];
-    var newMessages = [];
-    var conversationId = null;
-    var queryContacts = mod.getcontacts(user_id);
-    queryContacts.then((data) => {
-        newContacts = data.rows;
-    }).catch(err => console.log(err));  
+    var queryConv = mod.getmessages(res.locals.convId);
 
-    var queryConv = mod.getconvo(user_id, contact_id);
     queryConv
     .then((data) => {
-        conversationId = (data.rows[0]["conversation_id"]);
-        mod.getmessages(data.rows[0]["conversation_id"])
-        .then((data) => {
-            newMessages = data.rows;
-            newMessages.forEach(element => {
-                let date = element.date;
-                element.date = `${date.toLocaleString('default', { month: 'short' })} ${date.getDate()}`;
-                element.time = date.toLocaleString('default', { hour: 'numeric', hour12: true, minute: 'numeric'});
-            });
-            res.render('chat', {chatAssests: true, contact: newContacts, message: newMessages, convId: conversationId, sender: user_id});
-        })
-    }).catch(err => console.log(err));  
-
+        newMessages = data.rows;
+        newMessages.forEach(element => {
+            let date = element.date;
+            element.date = `${date.toLocaleString('default', { month: 'short' })} ${date.getDate()}`;
+            element.time = date.toLocaleString('default', { hour: 'numeric', hour12: true, minute: 'numeric'});
+        });
+        res.render('chat', {chatAssests: true, contact: res.locals.contacts, message: newMessages, convId: res.locals.convId, sender: user_id});
+    })
 }
 
-function loadChat(req,res,next) {
+function getContacts(req,res,next) {
     // let user_id = req.body.user_id;
     let user_id = 1;
     let dbQuery = mod.getcontacts(user_id);
     dbQuery.then((data) => {
-        res.render('chat', {chatAssests: true, contact: data.rows, });
+        res.locals.contacts = data.rows;
+        next();
+    }).catch(err => console.log(err));  
+}
+
+function getConvId(req,res,next) {
+    // let user_id = req.body.user_id;
+    // REPLACE WITH SESSIONS... 
+    //
+    //
+    //
+    //
+    //
+    //
+    var user_id = 1;
+    var contact_id = req.query.contactId;
+    if(contact_id == undefined){
+        contact_id = res.locals.contacts[0].id;
+    }
+    
+    var queryConv = mod.getconvo(user_id, contact_id);
+    queryConv
+    .then((data) => {
+        res.locals.convId = (data.rows[0]["conversation_id"]);
+        next();
     }).catch(err => console.log(err));  
 }
 
@@ -49,7 +67,8 @@ function newMessage(req,res,next) {
 }
 
 module.exports = {
-    loadChat: loadChat,
+    getContacts: getContacts,
+    getConvId: getConvId,
     loadConversation: loadConversation,
     newMessage: newMessage,
 }
