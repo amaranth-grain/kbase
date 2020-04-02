e447c41c8681525cc2d431e04d798ca131830af7
John Lee <johnlee46@gmail.com>
2020-04-02 10:40:45 -0700

    update chatdata
    updatechatdata

--- models/chatData.js
+++ models/chatData.js
@@ -1,7 +1,8 @@
 let db = require('../db/db');
 //gets a list of users that a user has conversations with 
 function getContacts(id){
-    return db.query('(select * from conversation left join users on users.id = conversation.second_user where first_user ='+id +' ) union all (select * from  conversation left join users on users.id = conversation.first_user where second_user = '+ id+')')
+    query = `(select * from conversation left join users on users.id = conversation.second_user where first_user =${id}) union all (select * from  conversation left join users on users.id = conversation.first_user where second_user = ${id})`
+    return db.query(query)
 
  
     /* same usage for a controller, need to loop through the data rows 
@@ -21,18 +22,19 @@ function getContacts(id){
 
 //get an individual conversation based on id and the other users id 
 function getConversation(id,contactid){
-    return db.query('select conversation_id from conversation where first_user =' + id + ' and second_user =' + contactid + ' or first_user=' +contactid+ 'and second_user=' + id);
+    query = `select conversation_id from conversation where first_user = ${id} and second_user = ${contactid} or first_user= ${contactid} and second_user=${id}`
+    return db.query(query);
 }
 
 
 //gets messages for the specified conversation id 
 function getMessages(conversation_id){
-    //return db.query('select * from message left join users on id = message.sender where conversation_id =' + conversation_id + ' order by date asc,time asc')
-    return db.query('select * from message left join users on id = message.sender where conversation_id = '+ conversation_id + ' order by timestamp asc')
+    query = `select * from message left join users on id = message.sender where conversation_id = ${ conversation_id} order by timestamp asc`
+    return db.query(query)
 }
 
 function getLatestMessage(conversation_id){
-    return db.query('select * from message left join users on id = message.sender where conversation_id = ' + conversation_id + ' order by timestamp desc limit 1')
+    return db.query('select * from message left join users on id = message.sender where conversation_id = ${ conversation_id + ' order by timestamp desc limit 1')
 }
 
 // sample sql input INSERT INTO message (conversation_id,sender,message,timestamp) VALUES (1,1 ,'testtest','2017-03-14 07:10:00');
@@ -43,7 +45,7 @@ function createMessage(conversationid,senderid,message,timestamp){
 
 //creates a conversation between the two users 
 function createConversation(id,receiverid){
-    return db.query('INSERT INTO conversation (first_contact,second_contact) VALUES (' + id + ','+receiverid +');')
+    return db.query('INSERT INTO conversation (first_contact,second_contact) VALUES (${ id + ',${receiverid +');')
 }
 
 
