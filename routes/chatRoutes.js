f212bced5071262790c52462ceffd64f2c002050
Stella Tran <stellatranthi@hotmail.com>
2020-04-01 23:43:08 -0700

    Chat now displays latest message

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
 
-router.get('/chat', chatController.getContacts, chatController.getConvId, chatController.loadConversation);
+router.get('/chat', chatController.getContacts, chatController.getLatestMessage, chatController.getConvId, chatController.loadConversation);
 
-router.post('/chat', chatController.getContacts, chatController.getConvId, chatController.loadConversation);
+router.post('/chat', chatController.getContacts, chatController.getLatestMessage, chatController.getConvId, chatController.loadConversation);
 
 router.post('/chat/newMessage', chatController.newMessage);
 
