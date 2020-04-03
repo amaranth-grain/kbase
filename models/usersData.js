e6906e9b08fc3631323f0bada842514d7822322a
John Lee <johnlee46@gmail.com>
2020-04-02 18:57:10 -0700

    added some functions for userdata
    added some functions for userdata

--- models/usersData.js
+++ models/usersData.js
@@ -1,8 +1,28 @@
 let db = require('../db/db');
 
 function addusers(name,about,url,dob,country,email,password) {
-    query = `Insert into users (name,about,imageurl,dob,country,email,password) VALUES ('${name}','${about}','${url}','${dob}','${country},${email},${password}')`
+    query = `Insert into users (name,about,imageurl,dob,country,email,password) VALUES ('${name}','${about}','${url}','${dob}','${country}','${email}','${password}')`
      db.query(query);
+}
+
+function createCredentials(email,password){
+    query = `Insert into users (email,password) VALUES ('${email}','${password}')`
+    db.query(query);
+}
+
+function addProfile(name,about,dob,country,id){
+    query = `update users set name = '${name}', about='${about}',dob='${dob}',country='${country}' where id = ${id};`
+    db.query(query)
+}
+
+function addPicture(imageurl,id){
+    query = `update users set imageurl = '${imageurl}' where id = ${id};`
+    db.query(query)
+}
+
+function getID(email){
+    query = `select id from users where email = '${email}';`
+    db.query(query)
 }
 
 function getAllusers() {
@@ -19,4 +39,8 @@ module.exports = {
     add : addusers,
     getall : getAllusers,
     getusers: getusers,
+    addprofile:addProfile,
+    createcredentials:createCredentials,
+    addpicture:addPicture,
+    getId:getID
 }
