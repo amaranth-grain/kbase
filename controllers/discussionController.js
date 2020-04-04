6cc41c9a2a6af5c2783e9b7ebc2b698de9a9dd5b
Stella Tran <stellatranthi@hotmail.com>
2020-04-04 16:29:54 -0700

    Created discussionController

---
+++ controllers/discussionController.js
@@ -0,0 +1,20 @@
+let mod = require('../models/discussionData');
+
+function getLatestDiscussion(req,res,next) {
+    var offset = 0;
+    var limit = 5;
+    
+    var queryConv = mod.selectTopicRange(offset, limit);
+
+    queryConv
+    .then((data) => {
+        console.log(data.rows)
+    }).catch((err) => console.log(err));
+    // }).catch(() => res.render('chat', {chatAssests: true, contacts: res.contacts, messages: [{"name": "Error: getting messages failed."}], convId: conservationId, sender: user_id}));
+
+}
+
+
+module.exports = {
+    getLatestDiscussion: getLatestDiscussion,
+}
