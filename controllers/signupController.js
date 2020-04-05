8f7eee24a9eb8aa58859aae62600510d3fca71f1
amaranth-grain <abstruse.amaranth@gmail.com>
2020-04-04 20:04:27 -0700

    working on signup

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -2,23 +2,32 @@ const mod = require("../models/usersData");
 
 module.exports = {
   signup: (req, res, next) => {
-    const fname = req.body.fname;
-    const lname = req.body.lname;
-    const email = req.body.email;
-    const password = req.body.password;
-    const password2 = req.body.passwordConfirm;
+    let user = {
+      fname: req.body.fname,
+      lname: req.body.lname,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.passwordConfirm,
+      about: "I am me",
+      imageurl: "https://randomuser.me/api/portraits/med/women/85.jpg",
+      dob: Date(),
+      country: "Canada",
+    };
     if (
-      fname != null &&
-      lname != null &&
-      email != null &&
-      password != null &&
-      password == password2
+      user.fname != null &&
+      user.lname != null &&
+      user.email != null &&
+      user.password != null &&
+      user.password == user.password2
     ) {
+      console.log(user.fname, user.email, user.password);
+      mod.createCredentials(user);
+      res.redirect(301, '/signup');
       // addUsers(name,about,imageurl,dob,country,email,password)
-      mod
-        .add(fname, "blah", "blah", "blah", "blah", email, password)
-        .then((result) => console.log(result))
-        .catch((err) => console.log(err));
+      // mod
+      //   .add(fname, "blah", "blah", "blah", "blah", email, password)
+      //   .then((result) => console.log(result))
+      //   .catch((err) => console.log(err));
     } else {
       console.log("Error with inputs");
     }
--- models/usersData.js
+++ models/usersData.js
@@ -5,19 +5,20 @@ function addUsers(name,about,imageurl,dob,country,email,password) {
      db.query(query);
 }
 
-function createCredentials(email,password){
-    query = `Insert into users (email,password) VALUES ('${email}','${password}')`
+function createCredentials(u){
+    console.log("Creating credentials...")
+    query = `Insert into users (name, email,password) VALUES ('${u.fname}', '${u.email}','${u.password}')`
     db.query(query);
 }
 
 function addProfile(name,about,dob,country,id){
     query = `update users set name = '${name}', about='${about}',dob='${dob}',country='${country}' where id = ${id};`
-    db.query(query)
+    db.query(query);
 }
 
 function addPicture(imageurl,id){
     query = `update users set imageurl = '${imageurl}' where id = ${id};`
-    db.query(query)
+    db.query(query);
 }
 
 function getId(email){
