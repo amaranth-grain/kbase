a66ded2f3bb0f8179bb99ae3062dd992ec6d43a1
amaranth-grain <abstruse.amaranth@gmail.com>
2020-04-07 00:26:11 -0700

    chat css and fixed mailer issue

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,4 +3,15 @@ function scrollToBottomOfChat(){
     chatDiv.scrollTop = chatDiv.scrollHeight;
 }
 
+const highlightSelectedElement = id => {
+    console.log("hi")
+    let el = document.getElementById(`c${id}`);
+    let chatContainers = document.getElementsByClassName("contactContainer");
+    for (container of chatContainers) {
+        container.style.backgroundColor = "#FFFFFF";
+    }
+    document.getElementById(id).click();
+    el.style.backgroundColor = "#F8F8F8";
+}
+
 scrollToBottomOfChat();
